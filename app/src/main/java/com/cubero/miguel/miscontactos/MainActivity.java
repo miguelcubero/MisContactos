0bf7763ed37b5b7f080155f306c1612cc0d9187b
Miguel Cubero <miguel.cubero.reyes@gmail.com>
2015-02-17 14:01:16 +0100

    COn Fragment de Menu pero Falla

--- app/src/main/java/com/cubero/miguel/miscontactos/MainActivity.java
+++ app/src/main/java/com/cubero/miguel/miscontactos/MainActivity.java
@@ -2,22 +2,22 @@ package com.cubero.miguel.miscontactos;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import com.cubero.miguel.miscontactos.utils.TabsPagerAdapter;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
-// IMplementamos la interfaz para que escuche cuando se pulse en las pestañas
-public class MainActivity extends Activity implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+// Implementamos la interfaz para que escuche cuando se pulse en las pestañas
+public class MainActivity extends Activity implements View.OnTouchListener  {
 
-    // Control de Fichas Tabs
-    private ViewPager viewPager;
-    // Va a ser una clase que va a administrar los componentes que vamos a tener dentro de nuestras pestañas
-    private TabsPagerAdapter adapter;
-    // Objeto de referencia a nuestro objeto ActionBar
-    private android.support.v7.app.ActionBar actionBar;
-    // Títulos de las pestañas que va a tener nuestro ActionBar
-    private String[] titulos = {"Crear Contacto","Lista Contactos"};
+
+    private ImageButton btnCrearContactos,btnListaContactos,btnEliminarContactos,btnSincronizar;
+    private CrearContactoFragment fragmentoCrear;
+    private ListaContactosFragment fragmentoLista;
+    private ActionBar actionBar;
 
 
 
@@ -25,64 +25,90 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Vie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inicializarTabs();
-
+        inicializaActionBar();
+        inicializaComponentes();
     }
 
 
 
-    private void inicializarTabs() {
-        // Sacamos el elemento raiz de nuestro nuevo xml limpito.
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        // Continuamos con las pestañas.
-        //actionBar = getActionBar();
-        actionBar = getSupportActionBar();
-
-        // El FragmentManager va a ser el administrador de los fragmentos que tenga nuestra actividad
-        adapter = new TabsPagerAdapter(getFragmentManager());
-        viewPager.setAdapter(adapter);
-        // Le decimos que la primera pestaña o tab no va a estar enabled por defecto
+    private void inicializaActionBar() {
+        actionBar = getActionBar();
         actionBar.setHomeButtonEnabled(false);
-        // Y que el modo de navegacion es por pestañas.
-        //actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+    }
 
-        // Agregamos los títulos de cada una de las pestañas
+    private void inicializaComponentes() {
+        btnCrearContactos = (ImageButton) findViewById(R.id.btn_crear_contacto);
+        btnCrearContactos.setOnTouchListener(this);
 
-        for(String nombre:titulos){
-            android.support.v7.app.ActionBar.Tab tab = actionBar.newTab().setText(nombre);
-            tab.setTabListener(this);
-            actionBar.addTab(tab);
+        btnEliminarContactos = (ImageButton) findViewById(R.id.btn_eliminar_contacto);
+        btnEliminarContactos.setOnTouchListener(this);
+
+        btnListaContactos = (ImageButton) findViewById(R.id.btn_lista_contactos);
+        btnListaContactos.setOnTouchListener(this);
+
+        btnSincronizar = (ImageButton) findViewById(R.id.btn_sincronizar);
+        btnSincronizar.setOnTouchListener(this);
+
+        cargarFragmento(getFragmentoLista());
+    }
+
+    private void cargarFragmento(Fragment fragmento) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.contenedor,fragmento);
+        transaction.commit();
+    }
+
+
+    //<editor-fold desc="INICIALIZACION BAJO DEMANDA (LAZY)">
+    public ListaContactosFragment getFragmentoLista() {
+        if( fragmentoLista == null ){
+            fragmentoLista = new ListaContactosFragment();
+        }
+        return fragmentoLista;
+    }
+
+
+    public CrearContactoFragment getFragmentoCrear() {
+        if( fragmentoCrear == null ){
+            fragmentoCrear = new CrearContactoFragment();
+        }
+        return fragmentoCrear;
+    }
+    //</editor-fold>
+
+    @Override
+    public boolean onTouch(View view, MotionEvent evt) {
+        ImageButton btn = (ImageButton) view;
+        // Comprobamos el estado del boton
+        int actionMasked = evt.getActionMasked();
+        switch (actionMasked){
+            case MotionEvent.ACTION_DOWN:
+                btn.setColorFilter(R.color.entintado_oscuro);
+                btn.invalidate();
+                cambiarFragmento(btn);
+                break;
+            case MotionEvent.ACTION_UP:
+                btn.clearColorFilter();
+                btn.invalidate();
+                break;
         }
 
 
-        // Le decimos al ViewPager que su auditor va a ser la página principal.
-        viewPager.setOnPageChangeListener(this);
+        return false;
     }
 
-    //<editor-fold desc="METODOS VIEW CHANGE LISTENER">
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    private void cambiarFragmento(View view) {
+        switch (view.getId()){
+            case R.id.btn_crear_contacto:
+                cargarFragmento(getFragmentoCrear());
+                break;
+            case R.id.btn_lista_contactos:
+                cargarFragmento(getFragmentoLista());
+                break;
+
+        }
+
 
     }
-
-    @Override
-    public void onPageSelected(int position) {
-        actionBar.setSelectedNavigationItem(position);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
-    //</editor-fold>
-
-    //<editor-fold desc="METODO TABS CHANGE LISTENER">
-    @Override
-    public void onTabSelected(android.support.v7.app.ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-        viewPager.setCurrentItem(tab.getPosition());
-    }
-
-    //</editor-fold>
-
 }
